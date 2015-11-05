64841c20b77cb9556ff37f56b5669c2d9d68433c
Ilkka Savolainen <ilkka.savolainen@qvantel.com>
2015-11-05 11:39:19 +0200

    Refactored and improved document list functionality.

---
+++ tms_support/testing/rpctest/ext_selfservice__consult_renewal_contractid.py
@@ -0,0 +1,19 @@
+import re
+import sys
+from selfservice_init import *
+from show_results import *
+
+
+# Definition
+s = xmlrpclib.ServerProxy(address,transport=p)
+def instruction():
+   print "Missing parameter: MSISDN"
+   print "Example: python ext_selfservice__consult_mnp_contractid.py 622689226"
+   sys.exit()
+
+if len(sys.argv) < 2: instruction()
+else:
+   var1 = sys.argv[1]
+
+q=s.ext.selfservice.consult_mnp_contractid('501',var1,'SMS')
+print_results(q)
