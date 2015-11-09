01c1a77011c3849f0793016e177fd5cc7d8ba933
Ilkka Savolainen <ilkka.savolainen@qvantel.com>
2015-11-09 10:22:41 +0200

    YBU-4821: fixed testing script.

--- tms_support/testing/rpctest/ext_selfservice__consult_renewal_contractid.py
+++ tms_support/testing/rpctest/ext_selfservice__consult_renewal_contractid.py
@@ -15,5 +15,5 @@ if len(sys.argv) < 2: instruction()
 else:
    var1 = sys.argv[1]
 
-q=s.ext.selfservice.consult_mnp_contractid('501',var1,'SMS')
+q=s.ext.selfservice.consult_renewal_contractid('501',var1,'SMS')
 print_results(q)
