7eeaf23027fcb6411b6b7df58714776c7c073bc9
Kari Aikas <kari.aikas@qvantel.com>
2015-11-10 15:44:59 +0200

    YPR-2969 added python test script for ext api

---
+++ tms_support/testing/rpctest/ext_selfservice__q25_add.py
@@ -0,0 +1,20 @@
+import re
+import sys
+from selfservice_init import *
+from show_results import *
+
+
+# Definition
+s = xmlrpclib.ServerProxy(address,transport=p)
+def instruction():
+   print "Missing parameter: MSISDN"
+   print "Example: python ext_selfservice__get_bundle_details.py 622689226"
+   sys.exit()
+
+if len(sys.argv) < 2: instruction()
+else:
+   var1 = sys.argv[1]
+
+q=s.ext.selfservice.get_bundle_details(var1)
+print_results(q)
+
