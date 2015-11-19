ff47231ae6500c83298e33cba2f622e3703ed855
Pasi Hautaniemi <pasi.hautaniemi@qvantel.com>
2015-11-19 17:07:31 +0100

    YDR-2044 Added missing arguments to py file.

--- tms_support/testing/rpctest/ext_selfservice__cancel_mnp_order.py
+++ tms_support/testing/rpctest/ext_selfservice__cancel_mnp_order.py
@@ -7,13 +7,15 @@ from show_results import *
 # Definition
 s = xmlrpclib.ServerProxy(address,transport=p)
 def instruction():
-   print "Missing parameter: OrderId"
-   print "Example: python ext_selfservice__cancel_mnp_order.py 10087533"
+   print "Missing parameter: OrderId MNP_Request_ID (SMS_cancel_code)"
+   print "Example: python ext_selfservice__cancel_mnp_order.py 70004575 005D0006171725025135103"
    sys.exit()
 
-if len(sys.argv) < 2: instruction()
+if len(sys.argv) < 3: instruction()
 else:
    var1 = int(sys.argv[1])
+   var2 = sys.argv[2]
+   var3 = sys.argv[3]
 
-q=s.ext.selfservice.cancel_mnp_order('501',var1,'005D0099635887081726601')
+q=s.ext.selfservice.cancel_mnp_order('501',var1,var2,var3)
 print_results(q)
