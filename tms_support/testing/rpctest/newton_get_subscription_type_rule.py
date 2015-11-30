8c431aff499dc6c44aef4e796d2979fc2ff606af
subhash sanjeevi <subhash.sanjeevi@qvantel.com>
2015-11-30 10:02:14 +0100

    STC dates to newton for prepaid to prepaid

--- tms_support/testing/rpctest/newton_get_subscription_type_rule.py
+++ tms_support/testing/rpctest/newton_get_subscription_type_rule.py
@@ -6,14 +6,15 @@ from show_results import *
 # Definition
 s = xmlrpclib.ServerProxy(address)
 def instruction():
-   print "Missing parameter: MsSeq"
-   print "Example: python newton_get_subscription_type_rule.py 10055111"
+   print "Example: python newton_get_subscription_type_rule.py 10055111 TARJ7 ''"
    sys.exit()
 
-if len(sys.argv) < 2: instruction()
+if len(sys.argv) < 4: instruction()
 else:
    var1 = int(sys.argv[1])
+   var2 = sys.argv[2]
+   var3 = sys.argv[3]
 
-p=s.newton.get_subscription_type_rule(var1)
+p=s.newton.get_subscription_type_rule(var1,var2,var3)
                               
 print_results(p)
