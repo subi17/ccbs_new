f2b8e85f0f02f7d207dbb674f6bc8221bf31973f
Janne Tourunen <janne.tourunen@qvantel.com>
2016-02-04 16:35:14 +0100

    Added working test file for staging usage

--- tms_support/testing/rpctest/dextra_update_order_status.py
+++ tms_support/testing/rpctest/dextra_update_order_status.py
@@ -12,7 +12,7 @@ fdate = xmlrpclib.DateTime(datetime.datetime.now().timetuple())
 s = xmlrpclib.ServerProxy(address, transport=p)
 def instruction():
    print "Missing parameter: OrderId LogisticStatus"
-   print "Example: python dextra_update_order_status.py 70003913 2"
+   print "Example: python dextra_update_order_status.py 70003913 1"
    sys.exit()
 
 if len(sys.argv) < 2: instruction()
@@ -24,13 +24,13 @@ q = s.dextra.update_order_status(
    {
       'Order_Id':var1,
       'timestamp':fdate,
-      'LO_Id':2,
+      'LO_Id':1,
       'LO_Description':"Netkia",
       'LO_Status_ID':var2,
       'LO_Status_Description':"",
-      'Courier_ID':4,
+      'Courier_ID':0,
       'Courier_Description': "ASM",
-      'Courier_Shipping_ID':""
+      'Courier_Shipping_ID':"ABC"
 #      'IMEI_COL':[{'MSDN':'342343','IMEI':'002344000000018'}],
 #      'delivery_address':{
 #         'region':'29',
