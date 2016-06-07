c7faf8bac22c41fe8fcaf8369da5b4f6aa07eadc
Tatu Lamminmäki <tatu.lamminmaki@qvantel.com>
2016-06-07 11:15:36 +0300

    Fixed tms Makefile.py and added new version of fcgi_agent

--- tms/Makefile.py
+++ tms/Makefile.py
@@ -17,111 +17,45 @@ nonp_source = ['script/' + x for x in os.listdir('script')]
 skip_timelog = False
 show_file = False
 
-log_level = '2'
-
-# example: log_entry_types = '4glmessages,4gltrace'
-log_entry_types = '4glmessages'
-daemon_state_dir = '../var/run'
-
-daemon_dirs = ['core/daemons']
-
 @target('test>test')
 def test(*a): pass
 
+@target
+def daemon(*a):
+    if len(parameters) < 1:
+        raise PikeException('Expected daemon name and instance as parameter')
+    daemon = parameters[0]
+    instance = parameters[1]
+    if instance == "?":
+        instance = ""
+    pid_file = '../var/run/d-' + daemon + instance + '.pid'
 
-def check_daemon(daemon):
-    return os.path.isfile(daemon.replace('.','/') + '.cls')
-
-def pid_file(daemon):
-    return daemon_state_dir+"/d-"+daemon+".pid"
-
-def start_daemon(daemon):
-
-    if not check_daemon(daemon):
-        print "That daemon ("+daemon+") does not exist"
+    if os.path.exists(pid_file):
+        print("The " + daemon + instance + " is already running")
         return
 
-    if os.path.isfile(pid_file(daemon)):
-        print "The daemon "+daemon+" is already running"
-        return
-
-    print "Starting "+daemon+"... "
-    file = open(pid_file(daemon), 'w')
+    print("Starting " + daemon + instance + "... ")
+    args = mpro + ['-b', '-p', 'gearbox/daemons/run_daemon.p',
+            '-clientlog', '../var/log/d-' + daemon + instance + '.log',
+            '-param', daemon + ',' + instance + ',../var/run',
+            '-T', '../var/tmp']
+    for pp in parameters[2:]:
+        if pp in databases:
+            args.extend(['-pf', '../db/progress/store/{0}.pf'.format(pp)])
+        else:
+            args.append(pp)
+    daemonpf = '../etc/pf/' + daemon + '.pf'
+    if os.path.exists(daemonpf):
+        args.extend([ '-pf' , daemonpf ])
+    file = open(pid_file, 'w')
     file.write(str(os.getpid()))
     file.close()
-    os.system(mpro[0] +' -b -p gearbox/daemons/run_daemon.p' +
-              ' -clientlog ../var/log/d-'+daemon+'.log' +
-              ' -pf ../etc/pf/d-'+daemon+'.pf' +
-              ' -logginglevel '+log_level+' -logentrytypes '+log_entry_types +
-              ' -param '+daemon+',,'+daemon_state_dir +
-              ' >> ../var/log/d-'+daemon+'-console.log &')
-    time.sleep(1)
-    if not os.path.isfile(pid_file(daemon)):
-        raise PikeException("** DAEMON "+daemon+" did not start.")
-
-
-def stop_daemon(daemon):
-
-    pidfile = pid_file(daemon)
-
-    if not check_daemon(daemon):
-        print "That daemon ("+daemon+") does not exist"
-        return
-    if not os.path.isfile(pidfile):
-        print "The daemon "+daemon+" is not running"
-        return
-    file = open(daemon_state_dir+"/d-"+daemon+".die", 'w')
-    file.write("")
-    file.close()
-
-    file = open(pidfile,"r")
-    pid = file.read()
-    file.close()
-
-    for i in [1, 2, 3, 2, 2]:
-        time.sleep(i)
-        if os.system('ps -p '+pid+ ">/dev/null"):
-            break
-
-    if not os.system('ps -p '+pid+ ">/dev/null"):
-        raise PikeException("** DAEMON "+daemon+" seems to hang. Try killing it")
-
-    print("The daemon "+daemon+" was stopped successfully.")
-
-def list_all_daemons():
-    for dir in daemon_dirs:
-        for daemon in glob.iglob(dir+"/*.cls"):
-            yield daemon.replace('/','.').replace('.cls','')
-
-@target
-def start(*a):
-    target = 'all'
-    if len(parameters) == 1:
-        target = parameters[0]
-
-    if target == 'all':
-        for daemon in list_all_daemons():
-            start_daemon(daemon)
-    else:
-        start_daemon(target)
-
-@target
-def stop(*a):
-    target = 'all'
-    if len(parameters) == 1:
-        target = parameters[0]
-
-    if target == 'all':
-        for daemon in list_all_daemons():
-            stop_daemon(daemon)
-    else:
-        stop_daemon(target)
+    os.execlp(args[0], *args)
 
 @target
 def build(*a):
     if len(parameters) != 1:
         raise PikeException('Expected build_dir as parameter')
-    open('Mc/version.i', 'wt').write(appversion)
     build_dir = parameters[0]
     for file in nonp_source + ['Makefile.py']:
         mkdir_p(os.path.dirname(os.path.join(build_dir, file)))
@@ -129,14 +63,7 @@ def build(*a):
     require('code.pl', [])
     shutil.move(myself + '.pl', build_dir + '/' + myself + '.pl')
 
-applhelp_dir = None
-
-def write_dirstate(filename):
-    info_file = open(filename, 'wt')
-    call(['hg', 'summary'], stdout=info_file)
-    call(['hg', 'st'], stdout=info_file)
-    call(['hg', 'diff'], stdout=info_file)
-    info_file.close()
+applhelp_dir = 'Help'
 
 @target('r')
 def code_pl(*a):
@@ -144,31 +71,16 @@ def code_pl(*a):
     if os.path.exists(myself + '.pl'):
         os.unlink(myself + '.pl')
     os.chdir('r')
-    write_dirstate('distinfo')
     if applhelp_dir:
         os.rename(applhelp_dir + '/applhelp.r', 'applhelp.r')
     call([dlc + '/bin/prolib', '../%s.pl' % myself, '-create'])
     for dir, _dirs, files in os.walk('.'):
         for file in files:
-            if file.endswith('.r') or file == 'distinfo':
+            if file.endswith('.r'):
                 call([dlc + '/bin/prolib', '../%s.pl' % myself, '-add',
                       os.path.join(dir[2:], file)])
     os.chdir('..')
     shutil.rmtree('r')
-
-@target
-def hotfix(*a):
-    inst_dir = '/apps' if len(parameters) != 1 else parameters[0]
-    inst_dir += '/' + appname + "-" + appversion
-    proc = Popen(['hg', 'branch'], stdout=PIPE)
-    branch = proc.communicate()[0].strip()
-    if branch != 'default' and not branch.endswith('-hotfix'):
-        inst_dir += '-' + branch
-    print('Hot-fixing ' + inst_dir)
-    open('Mc/version.i', 'wt').write(appversion)
-    require('code.pl')
-    write_dirstate(inst_dir + '/var/log/hotfix.log')
-    os.rename(myself + '.pl', os.path.join(inst_dir, myself, myself + '.pl'))
 
 @target
 def r(*a):
@@ -224,6 +136,7 @@ def _compile_all(format, target):
         source_files = list(classes_and_procedures(source_dir))
         procedure_file = make_compiler(format, source_files,
                                        show='name' if show_file else '.')
+
         if target:
             seen = []
             for dir in (os.path.dirname(target +'/' + x) for x in source_files):
@@ -231,7 +144,7 @@ def _compile_all(format, target):
                     mkdir_p(dir)
                     seen.append(dir)
 
-        comp = Popen(mpro + ['-pf', '../db/progress/store/all.pf',
+        comp = Popen(mpro + ['-pf', '../db/progress/store/all.pf', '-s', '1024',
                              '-b', '-p', procedure_file.name], stdout=PIPE)
         call('/bin/cat', stdin=comp.stdout)
         if comp.wait() != 0:
@@ -240,7 +153,7 @@ def _compile_all(format, target):
 
 @target
 def compile_one(match, *a):
-    '''(.*)\.p'''
+    '''(.*)\.(p|cls)'''
     print('Compiling file ' + match)
     sys.stdout.flush()
     procedure_file = make_compiler('COMPILE %s.', [match], show=None)
@@ -275,15 +188,42 @@ def clean(*a):
         os.unlink(file)
 
 
-run_module = 'misc/nn'
+run_module = 'Syst/tmslogin'
 run_extraargs = ['-e', '100', '-l', '2000', '-TB', '31', '-TM', '32',
                  '-rand', '2', '-Bt', '2500']
 @target
 def run(*a):
     args = ['-pf', '../db/progress/store/all.pf']
     args.extend(['-T', '../var/tmp'])
-    args = mpro + args + run_extraargs + ['-p', run_module + '.p']
+    args = mpro + args + run_extraargs + ['-clientlog', '../var/log/tms_ui.log', '-logginglevel', '4'] + ['-p', run_module + '.p']
     os.execlp(args[0], *args)
+
+@target
+def terminal(*a):
+    assert len(parameters) > 0, 'Which module to run?'
+    terminal_module = parameters[0]
+    module_base = os.path.basename(terminal_module)
+    if os.path.exists('../var/run/%s.pid' % module_base):
+        print('Lockfile %s.pid exists - aborting!' % module_base)
+        sys.exit(5)
+    fd = open('../var/run/%s.pid' % module_base, 'w')
+    fd.write(str(os.getpid()))
+    fd.close()
+
+    args = ['-T', '../var/tmp', '-p', terminal_module + '.p']
+    for pp in parameters[1:]:
+        if pp in databases:
+            args.extend(['-pf', '../db/progress/store/{0}.pf'.format(pp)])
+        else:
+            args.append(pp)
+    cmd = Popen(mpro + args)
+    while cmd.poll() is None:
+        try:
+            cmd.wait()
+        except KeyboardInterrupt:
+            cmd.send_signal(2)
+    os.unlink('../var/run/%s.pid' % module_base)
+    sys.exit(cmd.returncode)
 
 @target
 def batch(*a):
@@ -317,7 +257,59 @@ def batch(*a):
     sys.exit(cmd.returncode)
 
 @target
+def idbatch(*a):
+    assert len(parameters) > 0, 'Which module to run?'
+    batch_module = parameters[0]
+    module_base = os.path.basename(batch_module)
+    try:
+        batchid = int(parameters[1])
+        if batchid in databases:
+            raise IndexError
+    except ValueError:
+        print('Batch ID must be a number - aborting!')
+        sys.exit(5)
+    except IndexError:
+        print('No batch ID given - aborting!')
+        sys.exit(5)
+     
+    if os.path.exists('../var/run/%s_%s.pid' % (module_base, batchid)):
+        print('Lockfile %s_%s.pid exists - aborting!' % (module_base, batchid))
+        sys.exit(5)
+    fd = open('../var/run/%s_%s.pid' % (module_base, batchid), 'w')
+    fd.write(str(os.getpid()))
+    fd.close()
+
+    args = ['-T', '../var/tmp', '-b', '-p', batch_module + '.p']
+    for pp in parameters[2:]:
+        if pp in databases:
+            args.extend(['-pf', '../db/progress/store/{0}.pf'.format(pp)])
+        else:
+            args.append(pp)
+    try:
+        idx = args.index("-param")
+    except ValueError:
+        args.extend(["-param", 'batchid=%s' % batchid])
+    else:
+        try:
+            args[idx + 1] = 'batchid=%s,%s' % (batchid, args[idx + 1])
+        except IndexError:
+            args.append('batchid=%s' % batchid)
+
+    logfile = open('../var/log/%s_%s.log' % (module_base, batchid), 'a')
+    if not skip_timelog:
+        logfile.write(time.strftime('%F %T %Z') + ' {0}\n'.format('='*50))
+        logfile.flush()
+    cmd = Popen(mpro + args, stdout=logfile)
+    while cmd.poll() is None:
+        try:
+            cmd.wait()
+        except KeyboardInterrupt:
+            cmd.send_signal(2)
+    os.unlink('../var/run/%s_%s.pid' % (module_base, batchid))
+    sys.exit(cmd.returncode)
+
+@target
 def editor(*a):
     args = parameters or ['-pf', '../db/progress/store/all.pf']
-    args = mpro + args + ['-T', '../var/tmp']
+    args = mpro + args + ['-T', '../var/tmp'] + ['-s', '1024'] + ['-clientlog', '../var/log/tms_editor.log', '-logginglevel', '4']
     os.execlp(args[0], *args)
