575ccd84d8c370d6a0561a6d10a4ce193f649eb5
Antti Savolainen <antti.savolainen@qvantel.com>
2016-10-26 13:13:42 +0200

    restored config table dumps to be in sync with production

--- tms_support/utilities/tabledump/config_tables/FeeModel.d
+++ tms_support/utilities/tabledump/config_tables/FeeModel.d
@@ -41,8 +41,6 @@
 "CONTDATAMF" "Contrato Datos Service package" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "CONTDATAMFUPS" "IPL25 Data Upsell Bundle" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "CONTDATATOCONTD2" "IPL 25 to IPL 35 Upgrade Upsell" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
-"CONTDSL45MF" "Contrato DSL45 Monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 46
-"CONTDSL55MF" "Contrato DSL55 Monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 46
 "CONTF10" "CONTRATOF 10 monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "CONTF11MF" "Contrato Flat 11 monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "CONTF20" "CONTRATOF 20 monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
@@ -52,14 +50,9 @@
 "CONTF55" "CONTRATOF 55 monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "CONTF8MF" "Contrato Flat 8 monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "CONTFF2MF" "CONTRATOFF 2 monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
-"CONTFH45_50MF" "Contrato FH45_50 Monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 46
-"CONTFH55_300MF" "Contrato FH55_300 Monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 46
-"CONTFH55_50MF" "Contrato FH55_50 Monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 46
-"CONTFH65_300MF" "Contrato FH65_300 Monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 46
 "CONTM2MF" "CONTRATOM2 monthly fe" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "CONTMMF" "CONTRATOM monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "CONTRATO2" "Contrato2 Monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
-"CONTS10GBMF" "La Infinita 10 GB MonthlyFee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "CONTS12MF" "Contrato S 12 monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "CONTS15MF" "CONTRATOS 15 monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "CONTS16MF" "CONTRATOS 16 monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
@@ -67,7 +60,6 @@
 "CONTS21MF" "CONTRATOS 21 monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "CONTS25MF" "CONTRATOS 25 monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "CONTS26MF" "CONTRATOS 26 monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
-"CONTS2GBMF" "La Infinita 2 GB MonthlyFee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "CONTS30MF" "CONTRATOS 30 monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "CONTS32MF" "CONTRATOS 32 monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "CONTS35MF" "CONTRATOS 35 monthly fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
@@ -144,6 +136,7 @@
 "PAYTERM24_8" "Periodical contract installment" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "PAYTERM25" "Periodical contract installment" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "PAYTERMCG1E" "Commission fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
+"PAYTERMCGBC" "Commission fee CETELEM" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "PAYTERMCGBS" "Commission fee Sabadell" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "PMDUBFee" "Prepaid BONO8 Fee" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
 "POSMNP10" "Post paid - K800i BRONZE" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "" "1" 0
