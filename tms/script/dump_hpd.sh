24043b7b363905367ac52dffcbecfddfee3160e8
Tatu Lamminmäki <tatu.lamminmaki@qvantel.com>
2017-03-10 09:04:52 +0200

    Added brand to HPD header

--- tms/script/dump_hpd.sh
+++ tms/script/dump_hpd.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-TMSPATH=/apps/xfera
-RPATH=/tmsapps
-TERM=xterm
-
-export RPATH
-PROPATH=$RPATH,$TMSPATH
-
-for dir in tms/Ar tms/Func tms/Gwy tms/Help tms/Inv tms/Mc tms/Mf tms/Mm tms/Mnp tms/Rate tms/Syst tms/triggers tms/templates tools tools/stompAdapter
-do
-   PROPATH=$PROPATH,$TMSPATH/$dir
-done
-
-export TMSPATH
-export PROPATH
-export TERM
-
 die () {
     echo >&2 "$@"
     exit 1
@@ -23,4 +7,15 @@ die () {
 
 [ -n "$1" ] || die "Expected dumpid as parameter"
 
-mpro -b -q -pf /apps/xfera/tms/pf/batch.pf -p hpd_filedump_batch.p -clientlog /apps/yoigo/var/log/hpd_filedump_${1}.log -param ${1}
+if [ -z "$2" ]; then
+   tenant = "yoigo"
+else
+   tenant = $2
+
+[ "$tenant" != "super" ] || die "Super tenant is not allowed"
+
+echo "Using tenant $tenant"
+
+cd "${0%/*}"
+cd ../
+pike batch -- HPD/hpd_filedump_batch common ordercanal mobile counter star reratelog  -clientlog ../var/log/hpd_filedump_${1}_${tenant}.log -param ${1}
