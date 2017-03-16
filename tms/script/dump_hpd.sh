68d0ae42ca3f4b4f24b61970f7a720a1e7caaa83
Tatu Lamminmäki <tatu.lamminmaki@qvantel.com>
2017-03-16 16:30:07 +0200

    Fixed dump_hpd.sh

--- tms/script/dump_hpd.sh
+++ tms/script/dump_hpd.sh
@@ -7,15 +7,18 @@ die () {
 
 [ -n "$1" ] || die "Expected dumpid as parameter"
 
-if [ -z "$2" ]; then
-   tenant = "yoigo"
-else
-   tenant = $2
+tenant="yoigo"
+
+if [ -n "$2" ]; then
+   tenant=$2
+fi
 
 [ "$tenant" != "super" ] || die "Super tenant is not allowed"
 
 echo "Using tenant $tenant"
-
 cd "${0%/*}"
 cd ../
-pike batch -- HPD/hpd_filedump_batch common ordercanal mobile counter star reratelog  -clientlog ../var/log/hpd_filedump_${1}_${tenant}.log -param ${1}
+pike batch -- HPD/hpd_filedump_batch common ordercanal mobile counter star reratelog -clientlog ../var/log/hpd_filedump_${1}_${tenant}.log -param ${1}
+
+echo "Please check log file hpd_filedump_${1}_${tenant}.log"
+
