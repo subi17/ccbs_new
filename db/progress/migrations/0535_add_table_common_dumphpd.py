29b288919bbd912c7bab07e49ea2483ec935ce55
Tatu Lamminmäki <tatu.lamminmaki@qvantel.com>
2017-04-04 10:22:32 +0300

    Updated index area

--- db/progress/migrations/0535_add_table_common_dumphpd.py
+++ db/progress/migrations/0535_add_table_common_dumphpd.py
@@ -13,7 +13,7 @@ class AddTableDumpHPD(Migration):
         t.column('FinalTime', 'character', format="x(19)", initial="", max_width=38, label="Final Time", column_label="FinalTime", position=6, order=50, help="Final time of a dump DD.MM.YYYY HH:MM:SS")
         t.column('UnitsToDump', 'integer', format=">>>>>>>9", initial="0", max_width=4, label="Units to dump", column_label="UnitsToDump", position=7, order=60, help="How many unit to dump. Zero value means no restriction.")
         t.column('UnitType', 'character', format="x(12)", initial="days", max_width=24, label="Unit type", column_label="UnitType", position=8, order=70, help="The type of the unit (years, months, weeks, days, hours, minutes, seconds)")
-        t.index('DumpID', [['DumpID']], area="Sta_Index_2", primary=True, unique=True)
+        t.index('DumpID', [['DumpID']], area="Sta_Index_4", primary=True, unique=True)
 
     def down(self):
         self.drop_table('DumpHPD')
