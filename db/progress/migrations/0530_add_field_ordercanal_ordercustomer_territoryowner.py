bea04d88bd9b25c32a1c85cceb13bd82826d44a2
Tatu Lamminmäki <tatu.lamminmaki@qvantel.com>
2017-04-26 15:04:23 +0300

    Updated migrations

--- db/progress/migrations/0530_add_field_ordercanal_ordercustomer_territoryowner.py
+++ db/progress/migrations/0530_add_field_ordercanal_ordercustomer_territoryowner.py
@@ -6,7 +6,7 @@ class AddFieldTerritoryOwner(Migration):
 
     def up(self):
         t = self.alter_table('OrderCustomer')
-	t.column('TerritoryOwner', 'character', format="x(15)", initial="", max_width=16, label="TerritoryOwner", column_label="TerritoryOwner", position=74, order=1130, help="Territory Owner")
+        t.column('TerritoryOwner', 'character', format="x(15)", initial="", max_width=16, label="TerritoryOwner", column_label="TerritoryOwner", position=74, order=1130, help="Territory Owner")
 
     def down(self):
         t = self.alter_table('OrderCustomer')
