6d396793b405de425f734bcd8e60a8511af58d60
Tatu Lamminmäki <tatu.lamminmaki@qvantel.com>
2017-05-03 16:29:49 +0300

    Fixed character encoding to migration file

--- db/progress/migrations/0541_add_fields_common_invtext.py
+++ db/progress/migrations/0541_add_fields_common_invtext.py
@@ -10,7 +10,7 @@ class AddFieldDeliveryType(Migration):
         t.column('SchedulingPolicy', 'character', format="x(30)", initial="", max_width=60, label="SchedulingPolicy", column_label="SPolicy", position=28, order=270, help="From template. Name of scheduling policy. (optional)")
         t.column('SchedulingPriority', 'integer', format=">>>9", initial="", max_width=4, label="SchedulingPriority", column_label="SPriority", position=29, order=280, help="From template. Priority of message for scheduling in RabbitMQ. (optional)")
         t.column('Callbacks', 'character', format="x(60)", initial="", max_width=120, label="Callbacks", column_label="Callbacks", position=30, order=290, help="From template. List of callbacks to call (optional)")
-        t.column('ParamKeyValue', 'character', format="x(60)", initial="", max_width=120, label="ParamKeyValue", column_label="ParamKV", position=31, order=300, help="Â¤ separated parameter key=value list. (if present)")
+        t.column('ParamKeyValue', 'character', format="x(60)", initial="", max_width=120, label="ParamKeyValue", column_label="ParamKV", position=31, order=300, help="¤ separated parameter key=value list. (if present)")
         t.column('UseMMan', 'logical', format="Yes/No", initial="yes", max_width=1, label="Use message manager", column_label="UseMMan", position=32, order=310, help="Use message manager")
 
     def down(self):
