8f3ad8601e6f1601d684626b67fcdd3f1fa27667
Tatu Lamminmäki <tatu.lamminmaki@qvantel.com>
2017-09-01 16:18:22 +0300

    Fixed migration

--- db/progress/migrations/0557_add_field_ordercanal_ordercustomer_addressid.py
+++ db/progress/migrations/0557_add_field_ordercanal_ordercustomer_addressid.py
@@ -6,7 +6,7 @@ class AddFieldPRO(Migration):
 
     def up(self):
         t = self.alter_table('OrderCustomer')
-        t.column('AddressId', 'character', format="x(30)", initial="", max_width=60, label="AddressId", column_label="AddressId", position=77, order=1160, help="Address Id")
+        t.column('AddressId', 'character', format="x(30)", initial="", max_width=60, label="AddressId", column_label="AddressId", position=78, order=1170, help="Address Id")
 
     def down(self):
         t = self.alter_table('OrderCustomer')
