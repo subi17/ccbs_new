2eca66702e0f6abaf427d0ae668278a53788ba51
Tatu Lamminmäki <tatu.lamminmaki@qvantel.com>
2017-10-04 12:12:51 +0300

    Added depency support for srcpkg/Makefile.py

--- srcpkg/Makefile.py
+++ srcpkg/Makefile.py
@@ -11,16 +11,106 @@ exec(open(relpath + '/etc/make_site.py').read())
 
 tools_dir =  os.path.abspath('../tools/')
 
-@target
-def default(*a):
-    '''default|initialize'''
+depdict = {}
+
+def versiontuple(v):
+    filled = []
+    for point in v.split("."):
+        filled.append(point.zfill(8))
+    return tuple(filled)
+
+def getpackages():
+    # Get tuple list of packages (package, version)
+    # Store only the highest package version
+    packages = []
     for src in os.listdir('.'):
         if not src.endswith('.tar'): continue
         name, version = src[:-4].split('-', 1)
-        version_file = os.path.join(tools_dir, name, '.version')
+        ver = next((p for p, v in packages if p == name), None)
+        if not (ver and versiontuple(ver) > versiontuple(version)):
+            packages.append((name, version))
+    return packages
+
+def deprecursion(basepackage, package, packagelist):
+    if not package in depdict:
+        return
+
+    for deppackage in depdict[package]:
+        if deppackage == basepackage:
+            error.append('package "{0}" depends package "{1}" and vice versa'.format(deppackage, package))
+
+        elif not deppackage in packagelist:
+            packagelist.append(deppackage)
+            recur(basepackage, deppackage)
+    return
+
+
+@target
+def default(*a):
+    '''default|initialize'''
+
+    packages = getpackages()
+
+    depcount = {}
+    error = []
+    for package, version in packages:
+        version_file = os.path.join(tools_dir, package, '.version')
         if not os.path.exists(version_file) \
         or open(version_file).read().strip() < version:
-            require(src)
+            depcount[package] = 1
+            tar = tarfile.open('{0}-{1}.tar'.format(package,version))
+            deplist = []
+
+            try:
+                with tar.extractfile(".depedencies") as f:
+                    for line in f:
+                        if not line.beginswith('#'):
+                            try:
+                                deplist.append(tuple(line.strip(",").split(",")))
+                            except:
+                                error.append('"{0}": Unable to parse .depedencies line "{1}"'.format(package, line))
+                                continue
+            except KeyError:
+                pass
+
+            for deppackage, depversion in deplist:
+                if not deppackage in packages:
+                    error.append('"{0}": Depends package {1} which is not available'.format(package, deppackage))
+                    continue
+                availver = next(x for x in packages if x[0] == deppackage)[1]
+                if deppackage == package:
+                    error.append('"{0}": depends itself'.format(package))
+                elif versiontuple(depversion) > versiontuple(availver):
+                    error.append('"{0}": version "{1}" is needed for "{2}" but only "{3}" is available'.format(package, depversion, deppackage, availver))
+                else:
+                    if not package in depdict:
+                        depdict[package] = [deppackage]
+                    elif deppackage in depdict[package]:
+                        error.append('package "{0}" depends package "{1}" multiple times'.format(package, deppackage))
+                    else:
+                        depdict[package].append(deppackage)
+
+    if not error:
+        for package in depcount:
+            crosserror = []
+            packagelist = []
+            deprecursion(package, package, packagelist)
+            if crosserror:
+                error.append(crosserror)
+            else:
+                for pkg in packagelist:
+                    depcount[pkg] += 1
+
+    if error:
+        for item in error:
+            print(item)
+        raise PikeException('There were errors in packages')
+
+    installorder = sorted(depcount, key=depcount.get, reverse = True)
+
+    for package in installorder:
+        availver = next(x for x in packages if x[0] == package)[1]
+        require('{0}-{1}.tar'.format(package,availver))
 
 @target
 def build(*a):
