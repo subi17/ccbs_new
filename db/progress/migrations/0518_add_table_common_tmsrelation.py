eb2a5e02328637b960394df86c26145fefdf5a9d
Tatu Lamminmäki <tatu.lamminmaki@qvantel.com>
2018-01-03 09:09:54 +0200

    fp300: Fixed bug in migration file

--- db/progress/migrations/0518_add_table_common_tmsrelation.py
+++ db/progress/migrations/0518_add_table_common_tmsrelation.py
@@ -13,8 +13,8 @@ class AddTableTMSRelation(Migration):
         t.column('FromTime', 'datetime-tz', format="99-99-9999 HH:MM:SS.SSS+HH:MM", initial="?", max_width=12, label="From time", column_label="FromTime", position=6, order=50)
         t.column('ToTime', 'datetime-tz', format="99-99-9999 HH:MM:SS.SSS+HH:MM", initial="?", max_width=12, label="To time", column_label="ToTime", position=7, order=60)
         t.column('RelationType', 'character', format="x(20)", initial="", max_width=40, label="RelationType", column_label="RelationType", position=8, order=70, help="Type of the relation")
-        t.index('TableName', [['TableName'], ['KeyType'], ['KeyValue1'], ['ToTime', 'DESC']], area="Sta_Index_3", primary=True)
-        t.index('TableName', [['TableName'], ['KeyType'], ['KeyValue2'], ['KeyValue1'], ['ToTime', 'DESC']], area="Sta_Index_3", unique=True)
+        t.index('KeyValue1', [['TableName'], ['KeyType'], ['KeyValue1'], ['ToTime', 'DESC']], area="Sta_Index_3", primary=True)
+        t.index('KeyValue2', [['TableName'], ['KeyType'], ['KeyValue2'], ['KeyValue1'], ['ToTime', 'DESC']], area="Sta_Index_3", unique=True)
 
     def down(self):
         self.drop_table('TMSRelation')
