3b3f7445b621705140df04ed2450d5eced1e2f48
Tatu Lamminmäki <tatu.lamminmaki@qvantel.com>
2018-01-03 13:13:52 +0200

    fp300: Added new TMSRelation class

--- db/progress/migrations/0518_add_table_common_tmsrelation.py
+++ db/progress/migrations/0518_add_table_common_tmsrelation.py
@@ -8,13 +8,14 @@ class AddTableTMSRelation(Migration):
         t = self.table('TMSRelation', area="Sta_Data_128", multitenant="yes", label="Report Texts", dump_name="tmsrel", desc="General purpose relation table")
         t.column('TableName', 'character', format="x(25)", initial="", max_width=50, label="Table name", column_label="TableName", position=2, order=10)
         t.column('KeyType', 'character', format="x(30)", initial="", max_width=60, label="Key type", column_label="KeyType", position=3, order=20)
-        t.column('KeyValue1', 'character', format="x(30)", initial="", max_width=60, label="KeyValue1", column_label="KeyValue1", position=4, order=30)
-        t.column('KeyValue2', 'character', format="x(30)", initial="", max_width=60, label="KeyValue2", column_label="KeyValue2", position=5, order=40)
+        t.column('ParentValue', 'character', format="x(30)", initial="", max_width=60, label="Parent value", column_label="ParentValue", position=4, order=30)
+        t.column('ChildValue', 'character', format="x(30)", initial="", max_width=60, label="Child value", column_label="ChildValue", position=5, order=40)
         t.column('FromTime', 'datetime-tz', format="99-99-9999 HH:MM:SS.SSS+HH:MM", initial="?", max_width=12, label="From time", column_label="FromTime", position=6, order=50)
         t.column('ToTime', 'datetime-tz', format="99-99-9999 HH:MM:SS.SSS+HH:MM", initial="?", max_width=12, label="To time", column_label="ToTime", position=7, order=60)
         t.column('RelationType', 'character', format="x(20)", initial="", max_width=40, label="RelationType", column_label="RelationType", position=8, order=70, help="Type of the relation")
-        t.index('KeyValue1', [['TableName'], ['KeyType'], ['KeyValue1'], ['ToTime', 'DESC']], area="Sta_Index_3", primary=True)
-        t.index('KeyValue2', [['TableName'], ['KeyType'], ['KeyValue2'], ['KeyValue1'], ['ToTime', 'DESC']], area="Sta_Index_3", unique=True)
+        t.index('ParentValue', [['TableName'], ['KeyType'], ['ParentValue'], ['ToTime', 'DESC']], area="Sta_Index_3", primary=True)
+        t.index('ChildValue', [['TableName'], ['KeyType'], ['ChildValue'], ['ParentValue'], ['ToTime', 'DESC']], area="Sta_Index_3", unique=True)
+        t.index('ToTime', [['TableName'], ['KeyType'], ['ToTime', 'DESC']], area="Sta_Index_3")
 
     def down(self):
         self.drop_table('TMSRelation')
