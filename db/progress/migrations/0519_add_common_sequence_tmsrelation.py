5cfe9b5eb633a9f45e7be8e59d53327e8a76459f
Tatu Lamminmäki <tatu.lamminmaki@qvantel.com>
2018-01-05 16:53:50 +0200

    fp300: Fixed sequence name

--- db/progress/migrations/0519_add_common_sequence_tmsrelation.py
+++ db/progress/migrations/0519_add_common_sequence_tmsrelation.py
@@ -5,7 +5,7 @@ class AddSequences(Migration):
     database = "common"
 
     def up(self):
-        self.sequence('TMSRelation', initial=1, min_val=1, cycle_on_limit="no", increment=1)
+        self.sequence('TMSRelationID', initial=1, min_val=1, cycle_on_limit="no", increment=1)
 
     def down(self):
-        self.drop_sequence('TMSRelation')
+        self.drop_sequence('TMSRelationID')
