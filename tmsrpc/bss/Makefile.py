35f3999817557d6736aceca44f2c597529993fb5
Tatu Lamminmäki <tatu.lamminmaki@qvantel.com>
2018-04-05 22:04:11 +0300

    jsonrpc: Removed test mode parameter

--- tmsrpc/bss/Makefile.py
+++ tmsrpc/bss/Makefile.py
@@ -31,5 +31,5 @@ def run_agent(*a):
             '-clientlog', '../../var/log/%s_agent.%d.log' % \
             	          (parameters[0], os.getpid())]
 
-    args = mpro + args + extraargs + ['-b', '-p', 'profcgi/main.p', '-param', ',,,/home/talammin/temp/ootest']
+    args = mpro + args + extraargs + ['-b', '-p', 'profcgi/main.p', '-param', ',,']
     os.execlp(args[0], *args)
