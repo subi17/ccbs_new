82950c6cf0e5f80682d71942a62a9b6f63760943
Kari Hannula <kari.hannula@qvantel.com>
2018-04-17 12:09:35 +0200

    Migrations corrections.

--- db/progress/migrations/0524_add_field_common_customer_nwprofile.py
+++ db/progress/migrations/0524_add_field_common_customer_nwprofile.py
@@ -1,12 +1,12 @@
 from gearbox.migrations import Migration
 
-class AddFieldBundleType(Migration):
+class AddFieldNWProfile(Migration):
 
     database = "common"
 
     def up(self):
         t = self.alter_table('Customer')
-        t.column('NWProfile', 'integer', format=">9", initial="0", label="Network profile", column_label="NWProfile", position=126, max_width=1, order=1600, help="Network profile")
+        t.column('NWProfile', 'integer', format=">9", initial="0", label="Network profile", column_label="NWProfile", position=126, max_width=4, order=1600, help="Network profile")
 
     def down(self):
         t = self.alter_table('Customer')
