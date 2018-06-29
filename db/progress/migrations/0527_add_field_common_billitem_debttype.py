49dcfd030797fcec9a382cbe95da37b267aa9042
srinivasa vuddanti <srinivasa.vuddanti@qvantel.com>
2018-06-29 18:47:33 +0300

    errors corrected

--- db/progress/migrations/0527_add_field_common_billitem_debttype.py
+++ db/progress/migrations/0527_add_field_common_billitem_debttype.py
@@ -7,8 +7,10 @@ class AddFieldDebtType(Migration):
     def up(self):
         t = self.alter_table('BillItem')
         t.column('DebtType', 'character', format="x(12)", initial="", label="Debt Type", column_label="DebtType", position=27, max_width=24, order=400, help="Debt type")
-        
+         t.column('ProfitType', 'character', format="x(12)", initial="", label="Profit Type", column_label="ProfitType", position=28, max_width=24, order=410, help="Profit type")
+
     def down(self):
         t = self.alter_table('BillItem')
         t.drop_column('DebtType')
+        t.drop_column('ProfitType')
         
