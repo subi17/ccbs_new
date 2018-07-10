09b8769247da8e2ae8bcb4bf34108eb59c89e5c6
srinivasa vuddanti <srinivasa.vuddanti@qvantel.com>
2018-07-10 15:52:38 +0300

    PCIP-84 added fields to TMS DB & CUI

--- db/progress/migrations/0530_add_fields_common_taxclass.py
+++ db/progress/migrations/0530_add_fields_common_taxclass.py
@@ -6,8 +6,8 @@ class AddFieldTaxClass(Migration):
 
     def up(self):
         t = self.alter_table('TaxClass')
-        t.column('isActive', 'logical', format="yes/no", initial="no", label="Active", column_label="Active", position=4, max_width=1, order=30, help="Is TaxClass Active or not")
-        t.column('TCType', 'character', format="x(12)", initial="", label="is Active", column_label="is Active", position=5, max_width=24, order=40, help="Type of TaxClass")
+        t.column('isActive', 'logical', format="yes/no", initial="no", label="IsActive", column_label="IsActive", position=4, max_width=1, order=30, help="Is TaxClass Active or not")
+        t.column('TCType', 'character', format="x(12)", initial="", label="TCType", column_label="TCType", position=5, max_width=24, order=40, help="Type of TaxClass")
         t.column('amount', 'decimal', format="->>,>>9.99", initial="0", label="Amount", column_label="Amount", position=6, max_width=17, order=50, help="Tax Amount")
         
     def down(self):
