f6a81a02dad2b7285d93145ed702ca20143a2398
Koundinya Maddali <koundinya.maddali@qvantel.com>
2018-07-12 15:39:03 +0300

    PC:Added MsSeq to the OrderTopUp table

--- db/progress/migrations/0530_add_fields_ordercanal_ordertopup.py
+++ db/progress/migrations/0530_add_fields_ordercanal_ordertopup.py
@@ -8,10 +8,12 @@ class AddFieldOrderTopup(Migration):
         t = self.alter_table('OrderTopUp')
         t.column('OrderProductID', 'integer', format=">>>>>>9", initial="0", label="Product ID", column_label="Product ID", position=9, max_width=4, order=60, help="Product ID")
         t.column('DisplayAmount', 'decimal', format=">>>>9.99", decimals=2, initial="0", max_width=17, label="DispAmt", column_label="DispAmt", position=7, order=70, help="Disp Amount")
-        t.column('TopUpType', 'integer', format=">>>>>>9", initial="0", label="Type", column_label="Type", position=80, max_width=17, order=80, help="Topup Type")
+        t.column('TopUpType', 'integer', format=">>>>>>9", initial="0", label="Type", column_label="Type", position=80, max_width=4, order=80, help="Topup Type")
+        t.column('MsSeq', 'integer', format=">>>>>>9", initial="0", label="SubscrID", column_label="SubscrID", position=90, max_width=4, order=90, help="SubscrID")
         
     def down(self):
         t = self.alter_table('OrderTopUp')
         t.drop_column('OrderProductID')
         t.drop_column('DisplayAmount')
         t.drop_column('TopUpType')
+        t.drop_column('MsSeq')
