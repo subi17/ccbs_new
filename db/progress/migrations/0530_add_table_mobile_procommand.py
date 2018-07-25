5b70b4b56c92baa022dfe986865e7cbfa024d15d
Tatu Lamminmäki <tatu.lamminmaki@qvantel.com>
2018-07-25 16:21:36 +0300

    SAPC-49: Added procommand migration file

---
+++ db/progress/migrations/0530_add_table_mobile_procommand.py
@@ -0,0 +1,29 @@
+from gearbox.migrations import Migration
+
+class AddTableProCommand(Migration):
+
+    database = "mobile"
+
+    def up(self):
+        t = self.table('ProCommand', area="Sta_Data_32", multitenant="yes", label="BUS/BPM commands to SAPC", dump_name="procommand")
+        t.column('OrderId', 'integer', mandatory=True, format=">>>>>>>>9", initial="0", max_width=4, label="OrderId", column_label="OrderId", position=2, order=10, description="Order sequence number")
+        t.column('CreatedTS', 'datetime-tz', format="99/99/9999 HH:MM:SS.SSS+HH:MM", initial=self.unknown, max_width=12, label="Creation", position=3, order=20, help="Date and time when record is created")
+        t.column('ActivationTS', 'datetime-tz', format="99/99/9999 HH:MM:SS.SSS+HH:MM", initial=self.unknown, max_width=12, label="Activation", position=4, order=30, help="Date and time when procommand is sent to BUS/BPM")
+        t.column('CompletedTS', 'datetime-tz', format="99/99/9999 HH:MM:SS.SSS+HH:MM", initial=self.unknown, max_width=12, label="Completed", position=5, order=40, help="Date and time when BUS/BPM response is received")
+        t.column('ProCommandId', 'int64', format=">>>>>>>>>9", initial="0", max_width=8, label="ProCommand Id", position=6, order=50, help="Record Unique identifier")
+        t.column('MsRequest', 'integer', format=">>>>>>>>9", initial="0", max_width=4, label="Request ID", column_label="ID", position=7, order=60, help="Unique ID for request")
+        t.column('MsSeq', 'integer', mandatory=True, format=">>>>>>>9", initial="0", max_width=4, label="MobSub Sequence", column_label="SubSeq", position=8, order=70, help="Sequence for a subscription")
+        t.column('ProCommandStatus', 'integer', format=">>9", initial="0", max_width=4, label="Status", position=9, order=80, help="Status: 0 -  New, 1 - Ongoing, 2 - Done")
+        t.column('ProCommandTarget', 'character', format="x(8)", initial="", help="NB for Northbound, BPM for BPM", max_width=16, label="Target", position=10, order=90, description="NB for Northbound, BPM for BPM")
+        t.column('ProCommandTargetURL', 'character', format="x(70)", initial="", max_width=140, label="Target URL", position=11, order=100, help="Target URL")
+        t.column('ProCommandType', 'character', format="x(30)", initial="", help="Command type", max_width=60, label="Command Type", position=12, order=110, description="Command Type: add_upsell, change_imsi, ...")
+        t.column('Creator', 'character', format="x(12)", initial="", max_width=24, label="Creator", position=13, order=120, help="Who made this procommand request")
+        t.column('CommandLine', 'clob', clob_collation="BASIC", format="x(8)", initial=self.unknown, lob_size="10M", lob_bytes=10485760, lob_area="Y_Lob", position=14, clob_type=2, order=130, clob_codepage="UTF-8")
+        t.column('Response', 'clob', clob_collation="BASIC", format="x(8)", initial=self.unknown, lob_size="10M", lob_bytes=10485760, lob_area="Y_Lob", position=15, clob_type=2, order=140, clob_codepage="UTF-8")
+        t.index('Ix_ProCommandId', [['ProCommandId']], area="Sta_Index_2", primary=True, unique=True)
+        t.index('Ix_MsSeq', [['MsSeq'], ['ActivationTS', 'DESC']], area="Sta_Index_2")
+        t.index('Ix_OrderId', [['OrderId'], ['ActivationTS', 'DESC']], area="Sta_Index_2")
+        t.index('Ix_Status', [['ProCommandStatus'], ['ActivationTS']], area="Sta_Index_2")
+
+    def down(self):
+        self.drop_table('ProCommand')
