292ba08daab4e8a621e26334de2e3b187a62f6af
Tatu Lamminmäki <tatu.lamminmaki@qvantel.com>
2018-08-01 12:34:29 +0300

    sapc: Added bash scripts

---
+++ tms_support/sapc/tosapc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ -n "$1" ] || die "Expected file as parameter"
+
+tenant="yoigo"
+
+if [ -n "$2" ]; then
+   tenant=$2
+fi
+
+[ "$tenant" != "super" ] || die "Super tenant is not allowed"
+
+echo "Using tenant $tenant"
+
+pike -C /apps/yoigo/tms mbatch -- ../tms_support/sapc/customers_to_sapc common tenant=${tenant} -param ${1}
+
